3af48698e468d54b6d9cb20d1b78a8503660de48
michaelbrawer <michaelbrawer@me.com>
2017-12-07 21:36:09 -0800

    fixes navbar nested anchor error

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,26 +5,17 @@ import {Navbar, NavItem, Button, Icon, CardPanel, Col, Row, Footer} from 'react-
 
 const NavBar = (props) => {
   let nav = props.user ?
-      <div className="nav-wrapper">
-      <NavItem  >
-      <Link to="/topscores" className='NavBar-link' >TOP SCORES</Link>
-      </NavItem>
-      <NavItem>
-      <Link to="" className='NavBar-link' onClick={props.handleLogout} >LOG OUT</Link>
-      </NavItem>
-      <NavItem>
-      <Link to="" className='NavBar-welcome'>WELCOME, {props.user.name}</Link>
-      </NavItem> 
-      </div>
+
+      <ul>
+        <li><a href="" className='NavBar-link' onClick={props.handleLogout} >LOG OUT</a></li>
+        <li><span className='NavBar-welcome'>WELCOME, {props.user.name}</span></li>
+      </ul>
       :
-      <div className="nav-wrapper">
-      <NavItem>
-      <Link to="/login" className='NavBar-link'>LOG IN</Link>
-      </NavItem>
-      <NavItem>
-      <Link to="/signup" className='NavBar-link'>SIGN UP</Link>
-      </NavItem>
-      </div>
+      <ul>
+        <li><a href="/login">LOG IN</a></li>
+        <li><a href="/signup">SIGN UP</a></li>
+      </ul>
+      
   return (
     <Navbar className="HeaderFooter right" brand='HeartBleep' right>
       {nav}  
@@ -33,3 +24,22 @@ const NavBar = (props) => {
 };
 
 export default NavBar;
+
+
+
+{/* <NavItem  >
+<Link to="/topscores" className='NavBar-link' >TOP SCORES</Link>
+</NavItem>
+<NavItem>
+<Link to="" className='NavBar-link' onClick={props.handleLogout} >LOG OUT</Link>
+</NavItem>
+<NavItem>
+<Link to="" className='NavBar-welcome'>WELCOME, {props.user.name}</Link>
+</NavItem>  */}
+
+{/* <NavItem>
+<Link to="/login" className='NavBar-link'>LOG IN</Link>
+</NavItem>
+<NavItem>
+<Link to="/signup" className='NavBar-link'>SIGN UP</Link>
+</NavItem> */}
