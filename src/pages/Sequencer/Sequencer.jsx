5417dc0cebd09994d788f60ae29461e09dcd0a21
michaelbrawer <michaelbrawer@me.com>
2017-12-08 09:32:11 -0800

    prunes Link component from Sequencer

--- src/pages/Sequencer/Sequencer.jsx
+++ src/pages/Sequencer/Sequencer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
 import './Sequencer.css'
 import SequenceRow from '../../components/SequenceRow/SequenceRow';
 import Transport from '../../components/Transport/Transport';
