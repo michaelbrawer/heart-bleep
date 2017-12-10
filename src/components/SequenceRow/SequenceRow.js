fc1bb6c554ef963006973bd4b4eaa76ff113adb3
michaelbrawer <michaelbrawer@me.com>
2017-12-10 02:02:49 -0800

    adds polyrhythimical stepseq methods

--- src/components/SequenceRow/SequenceRow.js
+++ src/components/SequenceRow/SequenceRow.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
 import SequenceCell from '../SequenceCell/SequenceCell';
-import './SequenceRow.css';
 import {Col, Row, Input} from 'react-materialize'
+import './SequenceRow.css';
+import WebMidi from 'webmidi';
 
+const GRACE_NOTE_OFF = 100;
+const RESOLUTIONS = {
+  1: "1/16",
+  2: "1/8",
+  4: "1/4",
+  16: "1 Bar"
+}
 const DEFAULT_RESOLUTION = 4;
 
 const defaultPattern = () => {
@@ -54,9 +62,121 @@ class SequenceRow extends Component {
     });
   }
 
+  handleRemoveStep = (patternKey) => {
+    this.updatePattern(patternKey, pattern => {
+      let lastStep =
+      Object.keys(pattern.steps).length;
+      if (lastStep > 1){
+        delete pattern.steps[lastStep -1];
+      }
+      return pattern;
+    });
+  }
+
+  handleStepToggle(patternKey, stepKey) {
+    this.updatePattern(patternKey, pattern => {
+      if (pattern.steps != null && pattern.steps[stepKey] !== undefined) {
+        pattern.steps[stepKey] = pattern.steps[stepKey] == null
+          ? {play: true, velocity: 1}
+          : null;
+      }
+      return pattern;
+    });
+  }
+
+  handlePatternNoteChange(patternKey, e) {
+    this.updatePattern(patternKey, pattern => {
+      pattern.noteNumber = parseInt(e.target.value, 10);
+      return pattern;
+    });
+  }
+
+  updatePattern(patternKey, callback = () => {}) {
+    let newState = Object.assign({}, this.state);
+    let pattern = Object.assign({}, newState.patterns[patternKey]);
+
+    if (pattern != null) {
+      newState.patterns[patternKey] = callback(pattern);
+      this.setState(newState);
+    }
+  }
+
+  handleResolutionChange(value) {
+    this.setState({resolution: value});
+  }
+
+  handleAddPattern(e) {
+    e.preventDefault();
+    let newState = Object.assign({}, this.state);
+
+    newState.patterns.push(Object.assign({}, {
+      currentStep: -1,
+      noteNumber: 41,
+      steps: []
+    }));
+
+    this.setState(newState);
+  }
+
+  getLastStep(){
+    return this.state.patterns.reduce(function(memo, pattern){
+      return Math.max(Object.keys(pattern.steps).length, memo);
+    }, 0);
+  }
+
   getPatterns (){
     let totalSteps = new
     Array(this.getLastStep())
+  }
+
+  onClockTick(t0, t1, e) {
+    // Skip if outside of resolution
+    if (e.args.tick % this.state.resolution !== 0) {
+      return;
+    }
+
+    let newState = Object.assign({}, this.state);
+    let {selectedOutput, selectedChannel} = this.state;
+    let notes = [];
+
+    // Set individual pattern steps
+    newState.patterns.forEach(pattern => {
+      if (pattern.steps != null) {
+        let nextStep = parseInt(pattern.currentStep, 10) + 1;
+        let lastStep = Object.keys(pattern.steps).length;
+
+        // TODO avoid direct state mutation!
+        pattern.currentStep = lastStep > 0
+          ? nextStep % lastStep
+          : 0;
+
+        // Reset steps every 32 ticks
+        // TODO refactor this crap
+        if (e.args.tick % 32 === 0) {
+          pattern.currentStep = 0;
+        }
+
+        // Got note?
+        let activeStep = pattern.steps[pattern.currentStep];
+
+        if (activeStep != null) {
+          notes.push(pattern.noteNumber);
+        }
+      }
+    });
+
+    if (notes.length && selectedOutput != null) {
+      let duration = parseInt(e.args.duration * 1000, 10);
+      selectedOutput.playNote(
+        notes,
+        selectedChannel,
+        {
+          duration: WebMidi.time + duration - GRACE_NOTE_OFF,
+          velocity: 0.8
+        }
+      );
+    }
+    this.setState(newState);
   }
 
   render(){
