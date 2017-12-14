c868cb748f2021c3968e8b777b081124e358f3bf
michaelbrawer <michaelbrawer@me.com>
2017-12-14 11:28:23 -0800

    clean up sequencer row

--- src/components/SequenceRow/SequenceRow.jsx
+++ src/components/SequenceRow/SequenceRow.jsx
@@ -5,14 +5,14 @@ const SequenceRow = (props) => {
   function makeRow(v, i) {
     const channelClasses = v ? 'lighton' : 'lightoff';
     return (
-    
       <div
         className="stepbutton"
         data-channel={props.channelNum}
         data-stepindx={props.bside ? i + 13 : i}
         onClick={props.updateSeq}
         key={`c${v}s${i}`}>
-      <div className={channelClasses} /></div>
+        <div className={channelClasses} />
+      </div>
     );
   }
   return (
