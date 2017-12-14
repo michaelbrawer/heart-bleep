abfee86f0901e87185f557e8b0abc9eef96b6b52
michaelbrawer <michaelbrawer@me.com>
2017-12-13 19:46:03 -0800

    clean up sequence row component

--- src/components/SequenceRow/SequenceRow.jsx
+++ src/components/SequenceRow/SequenceRow.jsx
@@ -4,32 +4,20 @@ const SequenceRow = (props) => {
   function makeRow(v, i) {
     const channelClasses = v ? 'lighton' : 'lightoff';
     return (
-      
       <div
         className="stepbutton"
         data-channel={props.channelNum}
         data-stepindx={props.bside ? i + 13 : i}
         onClick={props.updateSeq}
-        key={`c${v}s${i}`}
-      >
+        key={`c${v}s${i}`}>
       <div className={channelClasses} /></div>
-      
     );
   }
-
   return (
     <div className="sequencerrow">
       {props.channel.map(makeRow, this)}
     </div>
-    
   );
 };
-
-// channelRow.propTypes = {
-//   channelNum: PropTypes.number.isRequired,
-//   bside: PropTypes.bool.isRequired,
-//   updateSeq: PropTypes.func.isRequired,
-//   channel: PropTypes.array.isRequired
-// };
 
 export default SequenceRow;
